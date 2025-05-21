660ff9132df4c1e7cbfa91343a92ee1f30c212a9
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-05-21 15:23:33 +0530

    adding login :memo:

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { IftaLabelModule } from 'primeng/iftalabel';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, InputTextModule, IftaLabelModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  bg = "images/bg.jpg";
+  bg_3 = "images/bg-3.jpg";
+  thrsl = "images/logo2.png";
 
+  value1: string | undefined;
+  value2: string | undefined;
 }
