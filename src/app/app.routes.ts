4a88a33de19d9e399337b8fbd4a9b4ba72c324a2
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-05-21 10:58:48 +0530

    adding pages/components :memo:

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:"login", pathMatch:"full"},
+    {path:'login', component:LoginComponent},
+    {path:'admin-dashboard', component:AdminDashboardComponent},
+    {path:'superAdmin-dashboard', component:SuperAdminDashboardComponent},
+    {path:'user-dashboard', component:UserDashboardComponent},
+];
